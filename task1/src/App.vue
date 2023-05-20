9175acb174f6e09724e2dac034d250eb147ee8fe
Rajat Mohadikar <rajatmohadikar97@gmail.com>
2023-05-21 01:59:35 +0530

    task1 and v-modelassignment commited

---
+++ task1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <child-component :data="myArr"></child-component>
+  </div>
+  
+</template>
+
+<script>
+import ChildComponent from './components/ChildComponent.vue'
+
+export default{
+  name: 'App',
+  components: {
+    ChildComponent
+  },
+  data(){
+    return {
+      myArr: [
+        {
+          studentName: "Jack",
+          studentClass: 6,
+          studentSection: "a"
+        },
+        {
+          studentName: "Tom",
+          studentClass: 6,
+          studentSection: "a"
+        }
+      ]
+    };
+  }
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ task1/src/components/ChildComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <table>
+    <thead>
+      <tr>
+        <th>Student Name</th>
+        <th>Student Class</th>
+        <th>Student Section</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+      <td>{{ item.studentName }}</td>
+      <td>{{ item.studentClass }}</td>
+      <td>{{ item.studentSection }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default{
+    props:{
+        data:{
+            type: Array
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
